d0ef572de56a2ec7a7b16b900800479224c531aa
Yi Lin Zhao <zyilin1998@outlook.com>
2020-02-22 10:41:09 -0500

    Basic Function

--- Scripts/scenes/Play.js
+++ Scripts/scenes/Play.js
@@ -16,23 +16,78 @@ var scenes;
 (function (scenes) {
     var Play = /** @class */ (function (_super) {
         __extends(Play, _super);
-        // PRIVATE INSTANCE MEMBERS
-        // PUBLIC PROPERTIES
         // CONSTRUCTOR
         function Play() {
             var _this = _super.call(this) || this;
+            _this.img1 = '1';
+            _this.img2 = '2';
+            _this.img3 = '3';
+            _this.img4 = '4';
+            _this.img5 = '5';
+            _this.img6 = '6';
+            _this.result = ["", ""];
+            _this.rollBotton = new objects.Button();
+            _this.diceResult1 = new objects.Label();
+            _this.diceResult2 = new objects.Label();
+            _this.dice1 = new objects.Button();
+            _this.dice2 = new objects.Button();
             _this.Start();
             return _this;
         }
         // PRIVATE METHODS
         // PUBLIC METHODS
+        Play.prototype.Roll = function () {
+            var roll = [];
+            for (var i = 0; i < 2; i++) {
+                //Generate random number between 1-6
+                roll[i] = util.Mathf.RandomRange(1, 6);
+                switch (roll[i]) {
+                    case 1:
+                        this.result[i] = this.img1;
+                        break;
+                    case 2:
+                        this.result[i] = this.img2;
+                        break;
+                    case 3:
+                        this.result[i] = this.img3;
+                        break;
+                    case 4:
+                        this.result[i] = this.img4;
+                        break;
+                    case 5:
+                        this.result[i] = this.img5;
+                        break;
+                    default:
+                        this.result[i] = this.img6;
+                        break;
+                }
+            }
+        };
+        Play.prototype.DisplayResult = function () {
+            this.removeChild(this.dice1);
+            this.removeChild(this.dice2);
+            this.removeChild(this.diceResult1);
+            this.removeChild(this.diceResult2);
+            this.dice1 = new objects.Button(config.Game.ASSETS.getResult(this.result[0]), 150, 200, true);
+            this.dice2 = new objects.Button(config.Game.ASSETS.getResult(this.result[1]), 150, 200, true);
+            this.diceResult1 = new objects.Label(this.result[0], "12", "Consolas", "#000000", 200, 200, true);
+            this.diceResult2 = new objects.Label(this.result[1], "12", "Consolas", "#000000", 201, 201, true);
+            this.addChild(this.dice1);
+            this.addChild(this.dice2);
+            this.addChild(this.diceResult1);
+            this.addChild(this.diceResult2);
+        };
         //initialize and instatiate
         Play.prototype.Start = function () {
+            this.rollBotton = new objects.Button(config.Game.ASSETS.getResult("rollButton"), 320, 430, true);
             this.Main();
         };
         Play.prototype.Update = function () {
         };
         Play.prototype.Main = function () {
+            var _this = this;
+            this.addChild(this.rollBotton);
+            this.rollBotton.on("click", function () { _this.Roll(); });
         };
         return Play;
     }(objects.Scene));
--- Scripts/scenes/Play.ts
+++ Scripts/scenes/Play.ts
@@ -3,14 +3,36 @@ module scenes
     export class Play extends objects.Scene
     {
         // PRIVATE INSTANCE MEMBERS
- 
-
+        
         // PUBLIC PROPERTIES
+        rollBotton:objects.Button;
 
+        img1: string = '1';
+        img2: string = '2';
+        img3: string = '3';
+        img4: string = '4';
+        img5: string = '5';
+        img6: string = '6';
+
+        diceResult1: objects.Label;
+        diceResult2: objects.Label;
+
+        roll1: number;
+        roll2: number;
+
+        dice1: objects.Button;
+        dice2: objects.Button;
+
+        result = ["",""];
         // CONSTRUCTOR
         constructor()
         {
             super();
+            this.rollBotton = new objects.Button();
+            this.diceResult1 = new objects.Label();
+            this.diceResult2 = new objects.Label();
+            this.dice1 = new objects.Button();
+            this.dice2 = new objects.Button();
 
             this.Start();
         }
@@ -18,11 +40,56 @@ module scenes
         // PRIVATE METHODS
 
         // PUBLIC METHODS
+        
+        public Roll():void {
+            var roll = [];
+            for (var i = 0; i < 2; i++){
+                //Generate random number between 1-6
+                roll[i] = util.Mathf.RandomRange(1,6);
+                switch(roll[i]){
+                    case 1:
+                        this.result[i] = this.img1;
+                        break;
+                    case 2:
+                        this.result[i] = this.img2;
+                        break;
+                    case 3:
+                        this.result[i] = this.img3;
+                        break;
+                    case 4:
+                        this.result[i] = this.img4;
+                        break;
+                    case 5:
+                        this.result[i] = this.img5;
+                        break;
+                    default:
+                        this.result[i] = this.img6;
+                        break;
+                }
+            }
+        }
 
+        
+        public DisplayResult():void{
+            this.removeChild(this.dice1);
+            this.removeChild(this.dice2);
+            this.removeChild(this.diceResult1);
+            this.removeChild(this.diceResult2);
+
+            this.dice1 = new objects.Button(config.Game.ASSETS.getResult(this.result[0]),150, 200, true);
+            this.dice2 = new objects.Button(config.Game.ASSETS.getResult(this.result[1]),150, 200, true);
+            this.diceResult1 = new objects.Label(this.result[0],"12", "Consolas", "#000000", 200, 200, true);
+            this.diceResult2 = new objects.Label(this.result[1],"12", "Consolas", "#000000", 201, 201, true);
+
+            this.addChild(this.dice1);
+            this.addChild(this.dice2);
+            this.addChild(this.diceResult1);
+            this.addChild(this.diceResult2);
+        }
         //initialize and instatiate
         public Start(): void 
         {
-            
+            this.rollBotton = new objects.Button(config.Game.ASSETS.getResult("rollButton"), 320, 430, true);
 
             
              this.Main();
@@ -35,7 +102,8 @@ module scenes
         
         public Main(): void 
         {
-
+            this.addChild(this.rollBotton);
+            this.rollBotton.on("click", ()=>{this.Roll()});
 
         }
 
